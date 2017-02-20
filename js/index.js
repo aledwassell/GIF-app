b906105fd23c8f59e247e193f3ed1e8df6fd44ea
Aled's Pro! <pc-user@pc-user-no-MacBook-Pro.local>
2017-02-21 07:48:36 +0900

    initial commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var api = 'http://api.giphy.com/v1/gifs/search?q='
+		,key = '&api_key=dc6zaTOxFJmzC'
+		,target = $('#target')
+		,input = $('input');
+
+	$('input').on('change', getData);
+	function getData() {
+		var url = api + input.val().replace(/\s/g, '+') + key;
+		$.getJSON( url ).done(function(giff){
+			gotData(giff);
+		});
+	};
+	function gotData(giff){
+		var index = 0;
+		while(index < giff.data.length){
+			var img = $('<img />', {
+			src: giff.data[index].images.fixed_height.url
+			});
+			img.prependTo(target);
+			index++;
+		};
+	};
+	getData();
+});
